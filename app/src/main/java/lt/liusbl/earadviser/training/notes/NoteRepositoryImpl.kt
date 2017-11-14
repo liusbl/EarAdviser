532edab23ffb62f9d657ec52e129bacd99213831
Liudvikas <liusbl@telesoftas.com>
2017-11-14 23:44:41 +0200

    Implement NoteRepository.kt#getBaseNotes() in NoteRepositoryImpl.kt with hardcoded values

--- app/src/main/java/lt/liusbl/earadviser/training/notes/NoteRepositoryImpl.kt
+++ app/src/main/java/lt/liusbl/earadviser/training/notes/NoteRepositoryImpl.kt
@@ -166,4 +166,21 @@ class NoteRepositoryImpl(private val scoreFactory: ScoreFactory) : NoteRepositor
 
     override fun getFundamentalNote(): Observable<Note> =
             Observable.just(Note("A", Note.A, 4, 440.00))
+
+    override fun getBaseNotes(): Observable<List<Note>> {
+        return Observable.just(listOf(
+                Note("C", Note.C, 4, 261.63),
+                Note("C#", Note.C_SHARP, 4, 277.18),
+                Note("D", Note.D, 4, 293.66),
+                Note("D#", Note.D_SHARP, 4, 311.13),
+                Note("E", Note.E, 4, 329.63),
+                Note("F", Note.F, 4, 349.23),
+                Note("F#", Note.F_SHARP, 4, 369.99),
+                Note("G", Note.G, 4, 392.00),
+                Note("G#", Note.G_SHARP, 4, 415.30),
+                Note("A", Note.A, 4, 440.00),
+                Note("A#", Note.A_SHARP, 4, 466.16),
+                Note("B", Note.B, 4, 493.88)
+        ))
+    }
 }
