9513d122fd70374c2c2b5635e98a324f0e72de50
Liudvikas <liusbl@telesoftas.com>
2017-11-16 01:59:34 +0200

    Create DependencyRetriever.kt which lazily initializes application components (for now just Gson and AppDatabase) and create it in BaseApplication#onCreate()

--- app/src/main/java/lt/liusbl/earadviser/base/BaseApplication.kt
+++ app/src/main/java/lt/liusbl/earadviser/base/BaseApplication.kt
@@ -2,4 +2,11 @@ package lt.liusbl.earadviser.base
 
 import lt.liusbl.earadviser.BuildTypeApplication
 
-class BaseApplication : BuildTypeApplication()
+class BaseApplication : BuildTypeApplication() {
+    lateinit var dependencyRetriever: DependencyRetriever private set
+
+    override fun onCreate() {
+        super.onCreate()
+        dependencyRetriever = DependencyRetriever(this)
+    }
+}
--- app/src/main/java/lt/liusbl/earadviser/base/DependencyRetriever.kt
+++ app/src/main/java/lt/liusbl/earadviser/base/DependencyRetriever.kt
@@ -1,9 +1,19 @@
 package lt.liusbl.earadviser.base
 
 import android.content.Context
+import com.google.gson.Gson
+import lt.liusbl.earadviser.base.database.DatabaseBundleInitializerImpl
+import lt.liusbl.earadviser.base.database.DatabaseFactoryImpl
+import lt.liusbl.earadviser.base.database.DatabaseInitializerImpl
 
 class DependencyRetriever(private val context: Context) {
-    val databaseHelper by lazy { }
+    val gson by lazy { Gson() }
+
+    val appDatabase by lazy {
+        val insertor = DatabaseBundleInitializerImpl(gson)
+        val initializer = DatabaseInitializerImpl(context, insertor)
+        DatabaseFactoryImpl(context, initializer).create()
+    }
 }
 
 val Context.dependencyRetriever: DependencyRetriever
