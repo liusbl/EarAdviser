a2035a80b6e4b918a599a429bee37fa6b0f2da6d
Liudvikas <liusbl@telesoftas.com>
2017-11-17 00:50:57 +0200

    Add initial field "notes" value of emptyList()

--- app/src/main/java/lt/liusbl/earadviser/training/score/Chord.kt
+++ app/src/main/java/lt/liusbl/earadviser/training/score/Chord.kt
@@ -2,4 +2,4 @@ package lt.liusbl.earadviser.training.score
 
 import lt.liusbl.earadviser.training.notes.Note
 
-data class Chord(val notes: List<Note>)
+data class Chord(val notes: List<Note> = emptyList())
