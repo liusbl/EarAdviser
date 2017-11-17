3da936149e873b2929a164d4e8dcdae7e13dc462
Liudvikas <liusbl@telesoftas.com>
2017-11-17 13:58:40 +0200

    Fix CollectionShufflerImpl.kt#shuffle()

--- app/src/main/java/lt/liusbl/earadviser/util/CollectionShufflerImpl.kt
+++ app/src/main/java/lt/liusbl/earadviser/util/CollectionShufflerImpl.kt
@@ -1,5 +1,11 @@
 package lt.liusbl.earadviser.util
 
+import java.util.*
+
 class CollectionShufflerImpl : CollectionShuffler {
-    override fun <T> shuffle(list: List<T>) = list.toList()
+    override fun <T> shuffle(list: List<T>): List<T> {
+        val mutableList = list.toMutableList()
+        Collections.shuffle(mutableList)
+        return mutableList.toList()
+    }
 }
