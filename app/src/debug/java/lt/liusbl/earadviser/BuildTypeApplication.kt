36cf2c860d99efe4bb39c2b52117ac6f551b2b7b
Liudvikas <liusbl@telesoftas.com>
2017-11-20 00:32:25 +0200

    Convert BuildTypeApplication.java to Kotlin

---
+++ app/src/debug/java/lt/liusbl/earadviser/BuildTypeApplication.kt
@@ -0,0 +1,24 @@
+package lt.liusbl.earadviser
+
+import android.app.Application
+
+import com.facebook.stetho.Stetho
+import com.facebook.stetho.timber.StethoTree
+import com.squareup.leakcanary.LeakCanary
+
+import dagger.android.support.DaggerApplication
+import timber.log.Timber
+
+open class BuildTypeApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        Stetho.initialize(Stetho.newInitializerBuilder(this)
+                .enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+                .enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this))
+                .build())
+        Timber.plant(Timber.DebugTree())
+        Timber.plant(StethoTree())
+
+        LeakCanary.install(this)
+    }
+}
