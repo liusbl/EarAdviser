7b2fdc5442922783368360aa0bf1393fe916176b
Liudvikas <liusbl@telesoftas.com>
2017-11-20 22:48:39 +0200

    Create SchedulerModule.kt with ComputationThread.kt, IoThread.kt and MainThread.kt annotation classes

---
+++ app/src/main/java/lt/liusbl/earadviser/base/scheduler/IoThread.kt
@@ -0,0 +1,7 @@
+package lt.liusbl.earadviser.base.scheduler
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.RUNTIME)
+annotation class IoThread
---
+++ app/src/main/java/lt/liusbl/earadviser/base/scheduler/SchedulerModule.kt
@@ -0,0 +1,33 @@
+package lt.liusbl.earadviser.base.scheduler
+
+import android.support.annotation.MainThread
+import dagger.Module
+import dagger.Provides
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Singleton
+
+@Module
+abstract class SchedulerModule {
+    @Module
+    companion object {
+        @JvmStatic
+        @MainThread
+        @Singleton
+        @Provides
+        fun provideUiScheduler(): Scheduler = AndroidSchedulers.mainThread()
+
+        @JvmStatic
+        @ComputationThread
+        @Singleton
+        @Provides
+        fun provideComputationScheduler(): Scheduler = Schedulers.computation()
+
+        @JvmStatic
+        @IoThread
+        @Singleton
+        @Provides
+        fun provideIoScheduler(): Scheduler = Schedulers.io()
+    }
+}
