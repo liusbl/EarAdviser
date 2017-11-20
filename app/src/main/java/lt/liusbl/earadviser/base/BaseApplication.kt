02b0aa54288ec851e445d0fa9842c5b9614db5fd
Liudvikas <liusbl@telesoftas.com>
2017-11-20 21:56:48 +0200

    Make BaseApplication.kt extends DaggerApplication()

--- app/src/main/java/lt/liusbl/earadviser/base/BaseApplication.kt
+++ app/src/main/java/lt/liusbl/earadviser/base/BaseApplication.kt
@@ -3,7 +3,7 @@ package lt.liusbl.earadviser.base
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
-class BaseApplication : DaggerApplication() {
+abstract class BaseApplication : DaggerApplication() {
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> =
             DaggerAppComponent.builder().create(this)
 }
